541df7ac276da49bc10bb03eb1ed3af384ad8c7c
Roland Bewick <roland.bewick@gmail.com>
2023-01-17 12:33:26 +0700

    chore: improve webln error handling

--- client/src/components/WebLN/GetPubKeyFromExtensionButton.tsx
+++ client/src/components/WebLN/GetPubKeyFromExtensionButton.tsx
@@ -1,35 +1,48 @@
-import { useEffect, useState } from "react";
-import { requestProvider, WebLNProvider } from "webln";
-import "./webln.css"
+/// <reference types="@webbtc/webln-types" />
+import "./webln.css";
 
 type GetPubKeyFromExtensionButtonProps = {
   setPubKey(pubKey: string): void;
-}
+};
 
-function GetPubKeyFromExtensionButton({setPubKey}: GetPubKeyFromExtensionButtonProps) {
-  return <button className="webln__button" onClick={(e) => {
-    (async () => {
-      let webln;
-      try {
-        webln = await requestProvider();
-      }
-      catch (error) {
-        console.error("Failed to request webln", error);
-        alert((error as Error).message);
-      }
-      if (webln) {
-        try {
-          const info = await webln.getInfo();
-          setPubKey(info.node.pubkey);
-        }
-        catch(error) {
-          console.error("Failed to get node info", error);
-          alert("Failed to request node info");
-        }
-      }
-    })();
-    e.preventDefault();
-  }}>Get from extension</button>;
+function GetPubKeyFromExtensionButton({
+  setPubKey,
+}: GetPubKeyFromExtensionButtonProps) {
+  return (
+    <button
+      className="webln__button"
+      onClick={(e) => {
+        (async () => {
+          if (window.webln) {
+            try {
+              await window.webln.enable();
+              try {
+                const info = await window.webln.getInfo();
+                if (info.node.pubkey) {
+                  setPubKey(info.node.pubkey);
+                } else {
+                  alert("Your current account does not have a public key");
+                }
+              } catch (error) {
+                console.error("Failed to get node info", error);
+                alert("Failed to request node info");
+              }
+            } catch (error) {
+              console.error("Failed to enable webln", error);
+              alert((error as Error).message);
+            }
+          } else {
+            alert(
+              "You don't have a Lightning extension yet. Try Alby at https://getalby.com/"
+            );
+          }
+        })();
+        e.preventDefault();
+      }}
+    >
+      Get from extension
+    </button>
+  );
 }
 
 export default GetPubKeyFromExtensionButton;
