5eb7e903d75a61ae50cf390426db769d9a944b61
channel.ninja <tidy.song2812@fastmail.com>
2023-01-17 10:49:11 +0100

    ✨ Add twitter social link

---
+++ client/src/components/Social/Social.tsx
@@ -0,0 +1,39 @@
+import "./social.css";
+
+const Social = () => {
+  return (
+    <div className="social">
+      <div className="social__item">
+        <a
+          className="social__link"
+          href="https://twitter.com/channel_ninja21"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="social__image social__image--twitter"
+            src="/twitter-logo-white.png"
+            alt="twitter logo"
+          />
+        </a>
+      </div>
+
+      <div className="social__item">
+        <a
+          className="social__link"
+          href="https://github.com/channelninja/channel.ninja"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="social__image"
+            src="/github-mark-white.svg"
+            alt="github logo"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Social;
