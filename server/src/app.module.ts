0075b3ed34502e0ce363b365913ead4a7962acd2
channel.ninja <tidy.song2812@fastmail.com>
2023-02-24 18:25:24 +0100

    chore(logs): add http logging

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerModule } from 'nestjs-pino';
+import pino from 'pino';
 import { AuthModule } from './auth/auth.module';
 import { FeesModule } from './fees/fees.module';
 import { GraphModule } from './graph/graph.module';
@@ -30,6 +32,28 @@ import { SuggestionsModule } from './suggestions/suggestions.module';
         };
       },
       inject: [ConfigService],
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        enabled: true,
+        stream: pino.destination({
+          // Buffer before writing
+          minLength: 4096,
+          // Asynchronous logging
+          sync: false,
+        }),
+        customProps: (_req, _res) => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-http-print',
+          options: {
+            colorize: true,
+            translateTime: true,
+            relativeUrl: true,
+          },
+        },
+      },
     }),
     StaticModule.forRoot(),
     ScheduleModule.forRoot(),
