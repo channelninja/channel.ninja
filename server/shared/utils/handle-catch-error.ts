3245d67c0b6f6e44f90081df80ee6ebb120f6dbe
channel.ninja <tidy.song2812@fastmail.com>
2023-03-10 23:05:26 +0100

    fix: remove error stack trace for production

--- server/shared/utils/handle-catch-error.ts
+++ server/shared/utils/handle-catch-error.ts
@@ -2,13 +2,19 @@ import { Logger } from '@nestjs/common';
 import { Environment } from 'server/core/config/environment.enum';
 
 export const handleCatchError = (error: unknown, logger: Logger, msg?: string) => {
-  if (error instanceof Error) {
-    if (process.env.NODE_ENV === Environment.Production) {
+  if (typeof error === 'string') {
+    logger.error({ err: { message: error } }, msg);
+
+    return;
+  }
+
+  if (typeof error === 'object') {
+    if ('stack' in error && process.env.NODE_ENV === Environment.Production) {
       delete error.stack;
     }
-
-    logger.error(error, msg);
   }
+
+  logger.error(error, msg);
 
   return error;
 };
